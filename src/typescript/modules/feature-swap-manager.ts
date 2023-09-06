106c2c02ff393efbcc3968903ea9cd9630a42d4b
Naspo <a.topaltsis@gmail.com>
2023-09-05 20:57:27 -0400

    Feature swap manager finished

--- src/typescript/modules/feature-swap-manager.ts
+++ src/typescript/modules/feature-swap-manager.ts
@@ -7,10 +7,45 @@ const generatorButton: HTMLButtonElement = document.getElementById(
 const validatorButton: HTMLButtonElement = document.getElementById(
   "validator-button"
 ) as HTMLButtonElement;
+const passwordGenerator: HTMLDivElement = document.getElementById(
+  "password-generator"
+) as HTMLDivElement;
+const passwordValidator: HTMLDivElement = document.getElementById(
+  "password-validator"
+) as HTMLDivElement;
 
 const hiddenClass: string = "hidden"; // applied to hide elements
 const buttonSelectedClass: string = "selected"; // applied to selected button
 
-export function generatorButtonClicked(): void {}
+// Checks and swaps clicked status of buttons (if necessary), calls swapFeature()
+export function generatorButtonClicked(): void {
+  if (generatorButton.classList.contains(buttonSelectedClass)) {
+    return;
+  }
+  validatorButton.classList.remove(buttonSelectedClass);
+  generatorButton.classList.add(buttonSelectedClass);
+  swapFeature();
+}
 
-export function validatorButtonClicked(): void {}
+// Checks and swaps clicked status of buttons (if necessary), calls swapFeature()
+export function validatorButtonClicked(): void {
+  if (validatorButton.classList.contains(buttonSelectedClass)) {
+    return;
+  }
+  generatorButton.classList.remove(buttonSelectedClass);
+  validatorButton.classList.add(buttonSelectedClass);
+  swapFeature();
+}
+
+// Swaps the features
+function swapFeature(): void {
+  // Show generator if it's hidden, and hide validator
+  if (passwordGenerator.classList.contains(hiddenClass)) {
+    passwordValidator.classList.add(hiddenClass);
+    passwordGenerator.classList.remove(hiddenClass);
+    // Show validator if it's hidden, and hide generator
+  } else {
+    passwordGenerator.classList.add(hiddenClass);
+    passwordValidator.classList.remove(hiddenClass);
+  }
+}
